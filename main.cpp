776ee730b920acc3517593813b49053257dc1a64
Luis Fernando Rojo Valdés <141747745+Blu-TEC@users.noreply.github.com>
2024-10-10 20:01:30 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,55 @@
 //Héctor Eduardo Ayala Gudiño A01638996
 #include "Error.h"
 #include "Registro.h"
-#include <iostream>
 
-using namespace std;
+// g++ main.cpp Registro.cpp Error.cpp && ./a.out
+// g++ main.cpp Error.cpp Registro.cpp && .\a.exe
 
-int main(){
+//Funcion con codigo de FreeCodeCamp para validar la fecha
+bool isValidIP(string busq) {
+
+    regex datePattern("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$");
+    if (!regex_match(busq, datePattern)) {
+        return false;
+    }
+    
+    return true;
+}
+
+int main()
+{
+    Registro r1;
+    r1.leerRegistro();
+    //r1.ordenar();
+    r1.crearArchivo();
+    
+    while(true){
+        string busqI, busqF;
+        do {
+            std::cout << "Teclee la IP de inicio de la busqueda (###.###.###.###): " << std::endl;
+            std::getline(cin, busqI);
+            if (!isValidIP(busqI)) {
+                std::cout << "IP no válida. Por favor, intente de nuevo." << std::endl;
+            }
+        } while (!isValidIP(busqI));
+        do {
+            std::cout << "Teclee la IP de fin de la busqueda (###.###.###.###): " << std::endl;
+            std::getline(cin, busqF);
+            if (!isValidIP(busqF)) {
+                std::cout << "IP no válida. Por favor, intente de nuevo." << std::endl;
+            }
+        } while (!isValidIP(busqF));
+        
+        r1.crearArchivoEspecifico(busqI, busqF);
+        cout<< "Archivo creado con exito"<<endl;
+        cout << "Desea realizar otra busqueda? (s/n)" << endl;
+        string respuesta;
+        cin >> respuesta;
+        if (respuesta != "s") {
+            break;
+        }
+        cin.ignore();
+    }
+
     return 0;
 }
